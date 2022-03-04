232e353e4e144206c37369c0d451eb132efeb3ab
Jack Pena <penajackc@gmail.com>
2022-03-04 10:44:08 -0700

    commit tree a9991d8ce9ce5b7fabe7f2c13196c1ab3ecd9f25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end 
+
+  get '/baked_goods/by_price' do
+    bgoods = BakedGood.all.order(:price).reverse
+    bgoods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    pricy = BakedGood.all.max_by {|good| good[:price]}
+    pricy.to_json
+  end
 
 end
